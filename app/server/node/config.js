4223726cd8941b17ead0252af3b1995ad7ebae50
techytimo <techytimo@blackpearl.localdomain>
2014-09-14 16:22:19 +0300

    google API key

--- app/server/node/config.js
+++ app/server/node/config.js
@@ -3,7 +3,7 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI || 'localhost',
   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook App Secret',
   FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Client Secret',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'gsM1EpvirwU0dr1ivcCmltYs',
   GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
   LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
   TWITTER_KEY: process.env.TWITTER_KEY || 'Twitter Consumer Key',
